571e3b4aa2ef72db1446113dab258b99066f8d04
ahmad <ahmad@localhost.localdomain>
2023-03-13 07:51:35 +0200

    public

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 const cors = require('cors')
+const path = require("path")
 // config
 const app = express()
 dotenv.config()
@@ -12,6 +13,12 @@ const todos = require('./routes/todos')
 // app
 app.use(express.json())
 app.use(cors())
+// deploy
+const __dirname1 = path.resolve()
+app.use(express.static(path.join(__dirname1, "/out")))
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname1, "out", "index.html"))
+)
 // app routes
 app.use('/api/users', users)
 app.use('/api/todos', todos)
